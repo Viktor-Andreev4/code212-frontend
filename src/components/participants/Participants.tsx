c7267b6641dc286152160ba77d7cc23558269d49
Vik <diswars99@gmail.com>
2023-08-01 13:11:27 +0300

    Participants page
    When you select an exam it shows you all the people who have participated in it. If there are no people, no participants are shown.

--- src/components/participants/Participants.tsx
+++ src/components/participants/Participants.tsx
@@ -1,7 +1,8 @@
 import {
-  IconButton,
-  Box,
   CloseButton,
+  SimpleGrid,
+  Heading,
+  Box,
   Flex,
   Icon,
   useColorModeValue,
@@ -11,18 +12,22 @@ import {
   useDisclosure,
   BoxProps,
   FlexProps,
-} from '@chakra-ui/react'
-import { FiHome, FiTrendingUp, FiCompass, FiStar, FiSettings, FiMenu, FiBook } from 'react-icons/fi'
-import { IconType } from 'react-icons'
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+} from '@chakra-ui/react';
+import { FiBook } from 'react-icons/fi';
 import { useEffect, useState } from 'react';
 import { getAllExams, getExamParticipants } from '../../services/client';
-import axios from 'axios';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-interface LinkItemProps {
-  name: string
-  icon: IconType
+interface NavItemProps extends FlexProps {
+  exam: Exam;
+  onExamSelect: (examId: number) => void;
 }
-
 interface Exam {
   id: number;
   name: string;
@@ -34,70 +39,23 @@ interface Participant {
   email: string;
 }
 
-const SidebarContent = ({ ...rest }: BoxProps) => {
-  const [exams, setExams] = useState<Exam[]>([]);
-
-  useEffect(() => {
-    const fetchExams = async () => {
-      try {
-        const response = await getAllExams();
-        setExams(response.data);
-      } catch (err) {
-        console.error("Failed to fetch exams:", err);
-      }
-    }
-
-    fetchExams();
-  }, []);
-  console.log("Exams:");
-  console.log(exams);
-
-  return (
-    <Box
-      bg={useColorModeValue('white', 'gray.900')}
-      borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}>
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-      </Flex>
-      {Array.isArray(exams) && exams.map((exam) => (
-        <NavItem key={exam.id} icon={FiBook} exam={exam}>
-          {exam.name}
-        </NavItem>
-      ))}
-    </Box>
-  )
+interface SidebarContentProps extends BoxProps {
+  examsProp: Exam[];
+  selectExam: (examId: number) => void;
+  onClose: () => void;
 }
 
-interface NavItemProps extends FlexProps {
-  icon: IconType
-  children: React.ReactNode
-  exam: Exam
-}
-const NavItem = ({ icon, children, exam, ...rest }: NavItemProps) => {
-  const [participants, setParticipants] = useState<Participant[]>([]);
-
-  const handleExamClick = async () => {
-    try {
-      const response = await getExamParticipants(exam.id);
-      setParticipants(response.data);
-    } catch (err) {
-      console.error(`Failed to fetch participants for exam ${exam.id}:`, err);
-    }
+const NavItem = ({ exam, onExamSelect, ...rest }: NavItemProps) => {
+  const handleClick = () => {
+    onExamSelect(exam.id);
   };
 
   return (
     <Box
-      as="a"
+      as="button"
+      onClick={handleClick}
       style={{ textDecoration: 'none' }}
-      _focus={{ boxShadow: 'none' }}
-      onClick={handleExamClick}>
+      _focus={{ boxShadow: 'none' }}>
       <Flex
         align="center"
         p="4"
@@ -110,55 +68,87 @@ const NavItem = ({ icon, children, exam, ...rest }: NavItemProps) => {
           color: 'white',
         }}
         {...rest}>
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: 'white',
-            }}
-            as={icon}
-          />
-        )}
-        {children}
+        <Icon
+          as={FiBook}
+          mr="4"
+          fontSize="16"
+          _groupHover={{
+            color: 'white',
+          }}
+        />
+        <Text>{exam.name}</Text>
       </Flex>
+    </Box>
+  );
+};
+
+const SidebarContent = ({ examsProp: exams, selectExam, onClose, ...rest }: SidebarContentProps) => {
+  return (
+    <Box
+      bg={useColorModeValue('white', 'gray.900')}
+      borderRight="1px"
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      
+      <Flex
+        h="20"
+        alignItems="center"
+        mx="8"
+        justifyContent="center"
+      >
+        <ChakraLink as={Link} to="/dashboard">
+          <Button colorScheme="teal">Dashboard</Button>
+        </ChakraLink>
+      </Flex>
+      
+      <Flex
+        h="20"
+        alignItems="center"
+        mx="8"
+        justifyContent="space-between"
+      >
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Exam list
+        </Text>
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+      
+      
+
+      {exams.map((exam) => (
+        <NavItem key={exam.id} exam={exam} onExamSelect={selectExam} />
+      ))}
     </Box>
   )
 }
 
-// const MobileNav = ({ onOpen, ...rest }: FlexProps) => {
-//   return (
-//     <Flex
-//       ml={{ base: 0, md: 60 }}
-//       px={{ base: 4, md: 24 }}
-//       height="20"
-//       alignItems="center"
-//       bg={useColorModeValue('white', 'gray.900')}
-//       borderBottomWidth="1px"
-//       borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-//       justifyContent="flex-start"
-//       {...rest}>
-//       <IconButton
-//         variant="outline"
-//         onClick={onOpen}
-//         aria-label="open menu"
-//         icon={<FiMenu />}
-//       />
-
-//       <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-//         Logo
-//       </Text>
-//     </Flex>
-//   )
-// }
-
 function Participants() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [exams, setExams] = useState<Exam[]>([]);
   const [participants, setParticipants] = useState<Participant[]>([]);
+
+  useEffect(() => {
+    getAllExams().then((response) => {
+      setExams(response.data);
+    }).catch((error) => {
+      console.error("Failed to fetch exams:", error);
+    });
+  }, []);
+
+  const handleExamSelect = (examId: number) => {
+    getExamParticipants(examId).then((response) => {
+      setParticipants(response.data);
+    }).catch((error) => {
+      console.error(`Failed to fetch participants for exam ${examId}:`, error);
+    });
+  };
 
   return (
     <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent display={{ base: 'none', md: 'block' }} />
+      <SidebarContent examsProp={exams} selectExam={handleExamSelect} onClose={onClose} display={{ base: 'none', md: 'block' }} />
       <Drawer
         isOpen={isOpen}
         placement="left"
@@ -167,14 +157,31 @@ function Participants() {
         onOverlayClick={onClose}
         size="full">
         <DrawerContent>
-          <SidebarContent />
+          <SidebarContent examsProp={exams} selectExam={handleExamSelect} onClose={onClose} />
         </DrawerContent>
       </Drawer>
-      {/* <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} /> */}
       <Box ml={{ base: 0, md: 60 }} p="4">
-        {participants.map((participant: Participant) => (
-          <p>{participant.firstName}</p>
-        ))}
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(400px, 2fr))'>
+          {participants.length === 0 ? (
+            <Text>No participants</Text>
+          ) : (
+            participants.map((participant: Participant) => (
+              <Card mb={5} mr={5}>
+                <CardHeader>
+                  <Heading size='md' textAlign="center"> {participant.firstName + " " + participant.lastName}</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text textAlign="center">Email: {participant.email}</Text>
+                  <Text textAlign="center">View a summary of all your customers over the last month.</Text>
+                </CardBody>
+                <CardFooter display="flex" justifyContent="center">
+                  <Button>View here</Button>
+                </CardFooter>
+              </Card>
+            ))
+          )}
+        </SimpleGrid>
+
       </Box>
     </Box>
   )
